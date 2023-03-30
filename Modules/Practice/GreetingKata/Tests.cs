5c24aa4d1cfe2325dd253962bc17c17242d08d2f
MightyBeast <zgpoter@gmail.com>
2023-03-30 17:18:24 +0300

    Refactoring.

--- Modules/Practice/GreetingKata/Tests.cs
+++ Modules/Practice/GreetingKata/Tests.cs
@@ -5,16 +5,23 @@ namespace codewars.Modules.Practice.GreetingKata;
 [TestFixture]
 public class Tests
 {
-    [Test(ExpectedResult = "Hello, my friend.")]
-    public string TestNullGreet() => new GreetingModule().Greet(null);
+    [Test, TestCaseSource(nameof(cases))]
+    public void TestGreetingModule(string[] names, string result) =>
+        Assert.AreEqual(result, new GreetingModule().Greet(names));
 
-    [TestCase("Bob", ExpectedResult = "Hello, Bob.")]
-    [TestCase("BOB", ExpectedResult = "HELLO BOB!")]
-    [TestCase("Bob", "Mike", ExpectedResult = "Hello, Bob and Mike.")]
-    [TestCase("Bob", "Fred", "Mike", ExpectedResult = "Hello, Bob, Fred, and Mike.")]
-    [TestCase("Bob", "FRED", "Mike", ExpectedResult = "Hello, Bob and Mike. AND HELLO FRED!")]
-    [TestCase("Bob", "Fred, Mike", ExpectedResult = "Hello, Bob, Fred, and Mike.")]
-    [TestCase("Bob", "\"Fred, Mike\"", ExpectedResult = "Hello, Bob and Fred, Mike.")]
-    public string TestGreetingModule(params string[] names) =>
-        new GreetingModule().Greet(names);
+    private static object[] cases =
+    {
+        new object[] { null, "Hello, my friend." },
+        new object[] { new[] { "Bob" }, "Hello, Bob." },
+        new object[] { new[] { "BOB" }, "HELLO BOB!" },
+        new object[] { new[] { "Bob", "Mike" }, "Hello, Bob and Mike." },
+        new object[] { new[] { "Bob", "Fred", "Mike" },
+            "Hello, Bob, Fred, and Mike." },
+        new object[] { new[] { "Bob", "FRED", "Mike" },
+            "Hello, Bob and Mike. AND HELLO FRED!" },
+        new object[] { new[] { "Bob", "Fred, Mike" },
+            "Hello, Bob, Fred, and Mike." },
+        new object[] { new[] { "Bob", "\"Fred, Mike\"" },
+            "Hello, Bob and Fred, Mike." },
+    };
 }
