b010cdf60d0466fe1a6308df9d988db3832c6332
MightyBeast <zgpoter@gmail.com>
2023-04-01 15:19:45 +0300

    Refactoring.

--- Modules/Fundamentals/TenMinuteWalk/Kata.cs
+++ Modules/Fundamentals/TenMinuteWalk/Kata.cs
@@ -4,18 +4,8 @@ public class Kata
 {
     public static bool IsValidWalk(string[] arr)
     {
-        if (arr.Length != 10) return false;
-
-        int[] position = new[] { 0, 0 };
-
-        foreach (string s in arr)
-        {
-            if (s == "n") position[1]++;
-            if (s == "s") position[1]--;
-            if (s == "e") position[0]++;
-            if (s == "w") position[0]--;
-        }
-
-        return position[0] == 0 && position[1] == 0;
+        return arr.Length == 10 &&
+            arr.Count(x => x == "n") == arr.Count(x => x == "s") && 
+            arr.Count(x => x == "w") == arr.Count(x => x == "e");
     }
 }
