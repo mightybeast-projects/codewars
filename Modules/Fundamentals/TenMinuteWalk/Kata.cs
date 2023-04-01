9b3f23eee4bc47c59e02294a84f07592cb4c777a
MightyBeast <zgpoter@gmail.com>
2023-04-01 15:20:43 +0300

    Refactoring.

--- Modules/Fundamentals/TenMinuteWalk/Kata.cs
+++ Modules/Fundamentals/TenMinuteWalk/Kata.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+
 namespace codewars.Modules.Fundamentals.TenMinuteWalk;
 
 public class Kata
@@ -8,4 +10,20 @@ public class Kata
             arr.Count(x => x == "n") == arr.Count(x => x == "s") && 
             arr.Count(x => x == "w") == arr.Count(x => x == "e");
     }
+}
+
+[TestFixture]
+public class Tests
+{
+    [Test, TestCaseSource(nameof(cases))]
+    public void TestValidWalk(string[] arr, bool result)
+        => Assert.AreEqual(result, Kata.IsValidWalk(arr));
+
+    private static object[] cases =
+    {
+        new object[] { new[] { "n" }, false },
+        new object[] { new[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s", }, true },
+        new object[] { new[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s", "w" }, false },
+        new object[] { new[] { "n", "s", "n", "s", "n", "s", "n", "s", "n" }, false },
+    };
 }
