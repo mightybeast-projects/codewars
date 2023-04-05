e7605b48cb4979187e1eb1312f217c69d615a2e3
MightyBeast <zgpoter@gmail.com>
2023-04-05 20:27:09 +0300

    Started working on decoder for secret message.

--- Modules/Practice/SecretMessage/Decoder.cs
+++ Modules/Practice/SecretMessage/Decoder.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace codewars.Modules.Practice.SecretMessage;
 
-public static class Encoder
+public class Decoder
 {
     private const string KEY = "abcdefghijklmnopqrstuvwxyz" +
                                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
@@ -11,7 +11,7 @@ public static class Encoder
     private const string SPECIAL = "!@#$%^&*()_+-";
     private static StringBuilder result;
 
-    public static string Encode(string str)
+    public static string Decode(string str)
     {
         result = new StringBuilder();
 
@@ -21,21 +21,20 @@ public static class Encoder
             if (SPECIAL.Contains(current))
                 result.Append(current);
             else
-                EncodeCharacter(current, i);
+            {
+                BigInteger keyIndex = KEY.IndexOf(current) + 1;
+                BigInteger multiplier = (BigInteger) Math.Pow(2, i + 1);
+                while (keyIndex % multiplier > 0)
+                {
+                    keyIndex += KEY.Length;
+                    System.Console.WriteLine(keyIndex);
+                }
+                BigInteger decodedI = keyIndex / multiplier;
+
+                result.Append(KEY[(int)decodedI - 1]);
+            }
         }
 
         return result.ToString();
-    }
-
-    private static void EncodeCharacter(char ch, int i)
-    {
-        int keyIndex = KEY.IndexOf(ch) + 1;
-        BigInteger encodedI = keyIndex * (BigInteger)Math.Pow(2, i + 1);
-        System.Console.WriteLine(ch + " " + keyIndex + " " + encodedI);
-
-        if (encodedI > KEY.Length)
-            encodedI = encodedI - encodedI / KEY.Length * KEY.Length;
-
-        result.Append(KEY[(int)encodedI - 1]);
     }
 }
--- Modules/Practice/SecretMessage/Tests.cs
+++ Modules/Practice/SecretMessage/Tests.cs
@@ -8,6 +8,9 @@ public class Tests
     [Test, TestCaseSource(nameof(encodingCases))]
     public string TestEncoding(string str) => Encoder.Encode(str);
 
+    [Test, TestCaseSource(nameof(decodingCases))]
+    public string TestDecoding(string str) => Decoder.Decode(str);
+
     private static TestCaseData[] encodingCases =
     {
         new TestCaseData("a").Returns("b"),
@@ -18,5 +21,15 @@ public class Tests
         new TestCaseData("Hello, world!").Returns("atC5kif6Pg1J!"),
         new TestCaseData("The quick brown fox jumps over the lazy dog")
             .Returns("yFNYhdmEdViBbxc40,ROYNxw.keKCVq464dLYMh8Vya")
+    };
+
+    private static TestCaseData[] decodingCases =
+    {
+        new TestCaseData("b").Returns("a"),
+        new TestCaseData("bd").Returns("aa"),
+        new TestCaseData("bdh").Returns("aaa"),
+        new TestCaseData("3dSpT,").Returns("Banana"),
+        new TestCaseData("3dSpT,22K").Returns("Bananabar"),
+        new TestCaseData("atC5kif6Pg1J!").Returns("Hello, world!")
     };
 }
